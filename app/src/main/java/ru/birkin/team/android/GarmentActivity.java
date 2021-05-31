55f7d057ed59095e002284b1116f06bee9e17786
sergus00@mail.ru <Ser008gus>
2021-05-31 07:47:45 +0500

    Слил свою ветку с Ваниной

--- app/src/main/java/ru/birkin/team/android/GarmentActivity.java
+++ app/src/main/java/ru/birkin/team/android/GarmentActivity.java
@@ -2,7 +2,11 @@ package ru.birkin.team.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class GarmentActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class GarmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garment);
+        add_listener();
+    }
+
+    public void add_listener () {
+        LinearLayout changeWashingSymbols = (LinearLayout) findViewById(R.id.change_washing_symbols);
+        changeWashingSymbols.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, manual_decryption.class);
+                context.startActivity(intent);
+            }
+        });
     }
 }
